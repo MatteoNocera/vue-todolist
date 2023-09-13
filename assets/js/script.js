ea51059ca14db75d2df140ee5f6f414262e97228
MatteoNocera <matteo_nocera@tiscali.it>
2023-09-13 22:18:40 +0200

    Bonus 1
    add @keyup. enter to permit to send input with enter

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,18 +12,23 @@ MILESTONE 2 Visualizzare a fianco ad ogni item ha una “x”: cliccando su di e
 
 MILESTONE 3 Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 
-Bonus: 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista 2- cliccando sul testo dell’item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
+Bonus: 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+2- cliccando sul testo dell’item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
 */
 const { createApp } = Vue
 
   createApp({
     data() {
       return {
+        keyValue : '',
+
         done : '',
+
         newTask : {
             text: '',
             done: false
         },
+
         tasks : [
             
         ]
@@ -31,9 +36,11 @@ const { createApp } = Vue
       }
     },
     methods: {
+
         removeTask(index) {
             this.tasks.splice(index, 1);
         },
+
         addTask() {
             console.log(this);
             if (this.addTask != '') {
